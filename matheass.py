9f1bb867e4e5a13ef8c68a7d3e87095930c527a8
Robert Hilbrich <robert@hilbri.ch>
2016-12-31 10:46:23 +0100

    Added more complex text-based tasks

--- matheass.py
+++ matheass.py
@@ -19,65 +19,116 @@ import sys
 def calc_add():
     s1 = random.randint(0, 100)
     s2 = random.randint(0, 100)
-    return (s1, s2, s1 + s2)
+    return ("%d + %d = ?  " % (s1, s2), s1 + s2)
+
+
+def calc_text_add():
+    s1 = random.randint(0, 100)
+    s2 = random.randint(0, 100)
+    return(u"\
+Graf Drakula hat Geburtstag. Er hat %d Brüder und %d Schwestern. \
+Alle möchte er gerne zu seiner Geburtstagsfeier einladen. Wieviele Gäste \
+kommen dann insgesamt zu seiner Geburtstagsfeier?\
+\n\n" % (s1, s2), s1 + s2)
 
 
 def calc_sub():
     m = random.randint(5, 100)
     s = random.randint(0, m)
-    return (m, s, m - s)
+    return ("%d - %d = ?  " % (m, s), m - s)
+
+
+def calc_text_sub():
+    m = random.randint(5, 100)
+    s = random.randint(0, m)
+    return(u"\
+Für seine Geburtstagsfeier hat Graf Drakula %d EUR gespart, um \
+für seine Freunde eine große Party zu veranstalten. Für die Torte \
+musste er %d EUR beim Bäcker bezahlen. Wieviel Geld hat er noch \
+für die Getränke übrig?\
+    \n\n" % (m, s), m - s)
 
 
 def calc_mul():
     f1 = random.randint(0, 10)
     f2 = random.randint(0, 10)
-    return (f1, f2, f1 * f2)
+    return (str(f1) + ' \xc2\xb7 '.decode('utf8') + str(f2) + " = ?  ",
+            f1 * f2)
+
+
+def calc_text_mul():
+    f1 = random.randint(0, 10)
+    f2 = random.randint(0, 10)
+    return(u"\
+Bei der Geburtstagsfeier von Graf Drakula kommen %d Gäste. Jeder Gast und \
+auch Drakula wollen das Geburtstagsmahl von %d Tellern essen. \
+Wieviele Teller müssen nach dem Essen von den Zwergen abgewaschen werden?\
+\n\n" % (f1 - 1, f2), f1 * f2)
 
 
 def calc_div():
-    div_is_correct = False
-    while not div_is_correct:
-        task = calc_mul()
-        if task[0] != 0:
-            div_is_correct = True
-    return (task[2], task[0], task[1])
+    f1 = random.randint(1, 10)
+    f2 = random.randint(0, 10)
+    p = f1 * f2
+    return("%d : %d = ?  " % (p, f1), f2)
+
+
+def calc_text_div():
+    f1 = random.randint(1, 10)
+    f2 = random.randint(0, 10)
+    p = f1 * f2
+    return(u"\
+Zur Geburtstagsfeier von Graf Drakula kommen %d Gäste. Die Geburtstagstorte \
+besteht aus %d Stücken. Wieviele Tortestücke kann jeder Partygast - und auch \
+Drakula - essen, so dass alle gleich viele Stücken Torte bekommen?\
+\n\n" % (f1 - 1, p), f2)
 
 
 def getTask():
 
-    # We need to make sure to have proper multiplication operator
-    operators = ('+', '-', '\xc2\xb7'.decode('utf8'), ':')
-
     # Find out, which operation we are now using
-    op_id = random.randint(0, 3)
+    op_id = random.randint(0, 7)
 
     # Trigger the appropriate sub-function for each op
     ops = {0: calc_add,
            1: calc_sub,
            2: calc_mul,
-           3: calc_div}
+           3: calc_div,
+           4: calc_text_add,
+           5: calc_text_sub,
+           6: calc_text_mul,
+           7: calc_text_div
+           }
 
     # Get the task and its result
     task = ops[op_id]()
 
-    return (task[0], operators[op_id], task[1], task[2])
+    return (task[0], task[1])
 
 
 def main():
-    print("Hallo Ritter Niklas!\n")
-    print("Du brauchst 100 Punkte, damit die Prinzessin gerettet wird!")
-    print("Deine Rüstung hält allerdings nur 5 Fehler aus ...\n")
-    print("Bestehst Du diese 10 Prüfungen, dann hast Du es geschafft.\n")
+
+    random.seed()
+
+    maxTasks = 20
+    maxFails = 5
+
+    print("\
+Hallo Ritter Niklas!\n\
+Du brauchst %d Punkte, damit die Prinzessin gerettet wird!\n\
+Deine Rüstung hält allerdings nur %d Fehler aus ...\n\
+Bestehst Du diese %d Prüfungen, dann hast Du es geschafft.\n"
+          % (maxTasks * 10, maxFails, maxTasks))
 
     score = 0
     fails = 0
 
-    for i in range(1, 11):
+    for i in range(1, maxTasks + 1):
         print("Prüfung Nummer %2d:" % i)
         task = getTask()
 
         while True:
-            print("%d %c %d = " % (task[0], task[1], task[2]), end="")
+            print(task[0] + u"Lösung: ", end="")
             r = sys.stdin.readline()
             try:
                 result = int(r)
@@ -85,28 +136,29 @@ def main():
             except:
                 print("Oh - Deine Eingabe war wohl keine Zahl.")
 
-        if (result == task[3]):
+        if (result == task[1]):
             score += 10
             print("Wunderbar - genau richtig! ", end="")
             print("Du bekommst 10 Punkte mehr. ", end="")
             print("Du hast nun %d Punkte!\n" % score)
         else:
             fails += 1
-            print("Leider falsch - richtig wäre: %d" % task[3])
-            if fails == 5:
-                print("Damit hast Du nun 5 Fehler gemacht und der Drache")
+            print("Leider falsch - richtig wäre: %d" % task[1])
+            if fails == maxFails:
+                print("Damit hast Du nun %d Fehler gemacht und der Drache" % maxFails)
                 print("knabbert schon an Deinen Haaren ...")
                 print("Versuche es doch noch einmal!")
+                return
             else:
                 print("Du hast nun %d Fehler gemacht - ohje ohje!" % fails)
                 print("So langsam kommt der Drache immer näher ... \n")
 
     print("")
 
-    if score == 100:
+    if score == maxTasks * 10:
         print("Jipiiiieeeehhh!")
         print("Die Prinzessin ist gerettet und Du hast den Drachen erlegt!")
-        print("Eine tolle Jagd Herr Ritter Niklas!")
+        print("Eine tolle Jagd Ritter Niklas!")
         print("")
         print("Bei so einem tollen Ergebnis freue ich mich schon ", end="")
         print("auf die nächste Jagd mit Dir!")
@@ -114,7 +166,7 @@ def main():
     else:
         print("Gratulation - Du hast bis hierhin durchgehalten und der Drache")
         print("hat Dich nicht gefressen. Deine >>%d Punkte<< reichen " % score)
-        print("aber nicht, um das Tür des Gefängnisses aufzusprengen.\n")
+        print("aber nicht, um die Tür des Gefängnisses aufzusprengen.\n")
         print("Versuche es doch noch einmal!")
 
 if __name__ == "__main__":
